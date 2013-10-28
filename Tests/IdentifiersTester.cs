7823ab030305ea74a48f40baaf70b359df613bf7
Kenny Evitt <kevitt@recp.com>
2013-10-28 14:16:35 -0400

    Added initial version of files.
    Case 308

---
+++ Tests/IdentifiersTester.cs
@@ -0,0 +1,30 @@
+﻿namespace Tests
+{
+    using System;
+
+    using NUnit.Framework;
+
+    using TransactSqlHelpers;
+
+    [TestFixture]
+    public class IdentifiersTester
+    {
+        [TestCase("Description", true)]
+        [TestCase("This should not be considered regular", false)]
+        [TestCase("EntityId", true)]
+        [TestCase("Jānis_Bērziņš", true)]
+        public void TestIfIdentifierIsRegular(string identifier, bool expectedIsRegularValue)
+        {
+            Assert.AreEqual(Identifiers.IsIdentifierRegular(identifier), expectedIsRegularValue);
+        }
+
+        [TestCase("Description", "Description")]
+        [TestCase("This should not be considered regular", "[This should not be considered regular]")]
+        [TestCase("EntityId", "EntityId")]
+        [TestCase("Jānis_Bērziņš", "Jānis_Bērziņš")]
+        public void TestIfNonRegularIdentifierIsDelimited(string identifier, string expectedValidIdentifierForm)
+        {
+            Assert.AreEqual(Identifiers.ValidIdentifier(identifier), expectedValidIdentifierForm);
+        }
+    }
+}
