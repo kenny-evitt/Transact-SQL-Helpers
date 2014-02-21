bdc8ff294215dc66bd06f42437d39fe46a6ad2ed
Kenny Evitt <kevitt@recp.com>
2014-02-21 16:57:03 -0500

    Added Scripts class with one helper method for parsing lines of a T-SQL script into batches and added a corresponding test class with one test method.
    Case 525

---
+++ Tests/ScriptsTester.cs
@@ -0,0 +1,39 @@
+﻿namespace Tests
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    using NUnit.Framework;
+
+    using TransactSqlHelpers;
+
+    [TestFixture]
+    public class ScriptsTester
+    {
+        [Test]
+        public void TestScriptWithTwoBatches()
+        {
+            string script = @"IF  EXISTS (SELECT * FROM sys.schemas WHERE name = N'administrator')
+DROP SCHEMA [administrator]
+GO
+
+CREATE SCHEMA [administrator] AUTHORIZATION [administrator]
+GO";
+
+            IEnumerable<string> scriptLines = Regex.Split(script, "\r\n|\r|\n");
+
+            List<string> expectedBatches =
+                new List<string>()
+                {
+                    @"IF  EXISTS (SELECT * FROM sys.schemas WHERE name = N'administrator')
+DROP SCHEMA [administrator]
+",
+                    @"
+CREATE SCHEMA [administrator] AUTHORIZATION [administrator]
+" };
+
+            Assert.IsTrue(expectedBatches.SequenceEqual(Scripts.GetBatches(scriptLines)));
+        }
+    }
+}
---
+++ TransactSqlHelpers/Scripts.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TransactSqlHelpers
+{
+    /// <summary>
+    /// Encapsulates various helper methods pertaining to Transact-SQL script files, particularly
+    /// script files typically used with SQL Server Management Studio and other SQL Server 
+    /// utilities.
+    /// </summary>
+    public static class Scripts
+    {
+        /// <summary>
+        /// Generates Transact-SQL batches, suitable for execution, from the lines of a 
+        /// Transact-SQL script.
+        /// </summary>
+        /// <param name="scriptLines"></param>
+        /// <returns></returns>
+        public static IEnumerable<string> GetBatches(IEnumerable<string> scriptLines)
+        {
+            List<string> batches = new List<string>();
+
+            StringBuilder nextBatch = new StringBuilder();
+
+            foreach (string line in scriptLines)
+            {
+                if (line.Trim().StartsWith("GO", System.StringComparison.InvariantCultureIgnoreCase))
+                {
+                    batches.Add(nextBatch.ToString());
+                    nextBatch.Clear();
+                }
+                else
+                {
+                    nextBatch.AppendLine(line);
+                }
+            }
+
+            return batches;
+        }
+    }
+}
