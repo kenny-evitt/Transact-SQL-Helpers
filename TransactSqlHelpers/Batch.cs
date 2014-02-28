00d95ccbd512587a63efeaf8285f1dad839634b5
Kenny Evitt <kevitt@recp.com>
2014-02-28 14:06:30 -0500

    Added classes for scripts and batches.
    Case 586

---
+++ TransactSqlHelpers/Batch.cs
@@ -0,0 +1,20 @@
+﻿namespace TransactSqlHelpers
+{
+    public class Batch
+    {
+        private readonly string _sql;
+
+        public string Sql
+        {
+            get
+            {
+                return _sql;
+            }
+        }
+
+        public Batch(string sql)
+        {
+            _sql = sql;
+        }
+    }
+}
